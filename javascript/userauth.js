b56a1cbad152884a7f7d94ccea560266a2947de2
ArcWare <ron.thomas2@student.tdsb.on.ca>
2015-08-01 21:15:41 -0400

    Moved userauth.js
    In order to organize <dir>:PATH,
    or the path-To Directory we move files,
    into specific containers.

---
+++ javascript/userauth.js
@@ -0,0 +1,19 @@
+var bcrypt = require('bcrypytjs'),
+	Q = require('q'),
+	config = require('./config.js'), //config file contains all tokens and private info
+	db = require('orchestrate')(config.db); //config.db holds Orcestrate token
+	
+var bcrypt-node = require('bcrypt-nodejs'),
+	 P = require('p'),
+	 functions = require("./functions.js") // functions file executes all scripts and commands
+	 api = require('twitter-api')(functions.api); // functions.api are the components 
+	 
+// used in local-signup strategy
+exports.localReg = function (username, password) {
+	var deferred = 	Q.defer();
+	var hash = bcrypt.hashSync(passowrd, 8);
+	var user = {
+		"username": username,
+		"password": password,
+	}
+}	
