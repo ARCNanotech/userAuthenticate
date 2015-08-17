994d559dc526fbaa2105662c238243441c0bee3c
Arcana Magus <ArcanaMagus@users.noreply.github.com>
2015-08-17 04:07:25 +0100

    Added an example server file

---
+++ javascript/server1.js
@@ -0,0 +1,17 @@
+// JavaScript source code
+var httpProxy = require('http-proxy');
+var proxy = httpProxy.createProxyServer(function(req, res) {
+    proxy.web(req, res, { target: 'http://hostmaster.arcware.edu' 
+        proxy.on('Error', function(e) {
+            catch.exception(e);
+            for (Error != 1; e <= exception) {
+                exception(e) == false;
+                return ExceptionInformation;
+                if (Error = 0) {
+                    exception(e) == true;
+                    onreset(e, exception) {
+                        close.httpProxy;
+                    }
+                }
+            }
+});
