be65f9d58707561b839912591dca18155c1212e0
GHOST <GHOST@users.noreply.github.com>
2015-08-21 02:04:58 +0100

    Updated javascript\private_members.js

--- javascript/private_members.js
+++ javascript/private_members.js
@@ -6,4 +6,13 @@ FormValidator = function () {
 
     // public data member (property)
     this.digits = "0123456789";
+
+    // private data function (method)
+    var _isEmpty = function (s) { return ((ss == null) || (s.length == 0)) };
+
+    // public data function (method)
+    this.isEmpty = function (s) { return ((s == null) || (s.length == 0)) };
 }
+
+
+    
