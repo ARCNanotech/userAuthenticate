eb7b3cb4557c8edbcf3ee11bcd9156b47dc2eb0b
GHOST <GHOST@users.noreply.github.com>
2015-08-21 01:46:57 +0100

    Added javascript\private_members.js

---
+++ javascript/private_members.js
@@ -0,0 +1,9 @@
+// JavaScript source code
+FormValidator = function () {
+
+    //private data member (property)
+    var _digits = "0123456789";
+
+    // public data member (property)
+    this.digits = "0123456789";
+}
