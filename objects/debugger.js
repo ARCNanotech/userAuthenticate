5facc272076fbb471f36c6192020a3d54c77ed26
ArcanaMagus <ArcanaMagus@users.no-reply.github.com>
2015-08-30 05:57:08 +0000

    Adding some comments to objects/debugger.js

--- objects/debugger.js
+++ objects/debugger.js
@@ -2,5 +2,7 @@
 
 if(!window.config) var  config {};
 config.Debug {
-    maxPopups: 5            
+    maxPopups: 5            /*use default setting of popup(or)error alerts before prompt
+    to continue
+    */
 };
