086463de5575fcc6e5674a2ef991a87160154254
ARC.Nanotech <ArcanaMagus@users.noreply.github.com>
2015-09-30 15:37:06 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // JavaScript source code
 exports.download = require('./lib/userauth.js')
-modules.exports = ('./REST-API.js')'
+modules.exports = ('./REST-API.js')
 exports.download = require('./config.js')
-module.exports = ('./lib/browser.js')
+modules.exports = ('./lib/browser.js')
