02955add61ffe96257de7bd3b9c696a4d29f3cdf
ARC.Nanotech <ArcanaMagus@users.noreply.github.com>
2015-10-01 11:53:26 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // JavaScript source code
-exports.download = require('./lib/userauth.js')
-modules.exports = ('./REST-API.js')
-exports.download = require('./lib/config.js')
-modules.exports = ('./lib/browser.js')
-exports.download = require('./lib/branch.js')
+var modules = {'prototype': 1, 'type': 'ParseFileOperator'
+
+exports.download = require('./lib/userauth.js');
+modules.exports = ('./REST-API.js');
+exports.download = require('./lib/config.js');
+modules.exports = ('./lib/browser.js');
+exports.download = require('./lib/branch.js');
