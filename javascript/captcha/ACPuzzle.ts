c5518b498e5b8bd19b280f55533f6561eeadd85d
ArcanaMagus <ArcanaMagus@users.no-reply.github.com>
2015-10-03 19:50:11 +0000

    Mod binary file Mod TypeScript file

--- javascript/captcha/ACPuzzle.ts
+++ javascript/captcha/ACPuzzle.ts
@@ -1,11 +1,17 @@
 var ckey: any;
 var protocol: any;
 var apiserver: string;
+<<<<<<< HEAD
 var mediaserver: any;
 var magic: any;
 var chalapi: any;
+=======
+var mediaserver: string;
+var magic: string;
+var chalapi: string;
+>>>>>>> Mod files for upcoming patch
 var chalstamp: any;
-var lang: any;
+var lang: number;
 var size: any;
 var theme: any;
 var type: any;
