f508942de1bb84f72387a0a579c8bde82b4b6204
ArcanaMagus <ArcanaMagus@users.no-reply.github.com>
2015-10-08 01:58:20 +0000

    Modify file server1.ts

--- javascript/server1.ts
+++ javascript/server1.ts
@@ -1,3 +1,7 @@
-var httpProxy: any;
-var component: any;
-var APIServer: any;
+var httpProxy:	any;
+var component:	any;
+var APIServer:	any;
+var httpsServer:	any;
+var proxy:	any;
+var mainline:	any;
+var axis:	any;
