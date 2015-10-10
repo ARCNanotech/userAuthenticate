8b3d4fe0f4cca39e54d495d7ced0e87a4b620c0e
ArcanaMagus <ArcanaMagus@users.no-reply.github.com>
2015-10-10 23:02:03 +0000

    Add content to file property.cpp

--- property.cpp
+++ property.cpp
@@ -1,1 +1,6 @@
-[[]]
+<include stdio.h>
+
+
+project element static[object(slider) {
+	slider.static.Movable.object(for {user::prefs} meta::element)
+} if [[element.slider: IOerror(pre: set, re-set: center)].post:'./makefile'];
