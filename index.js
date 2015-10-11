e05d3b740a936f594a7f14c0fb74ff70d588b366
ArcanaMagus <ArcanaMagus@users.no-reply.github.com>
2015-10-11 01:36:16 +0000

    Pre: Add index javasscript file Post: Update Matrix in library folder='/lib'

--- index.js
+++ index.js
@@ -2,7 +2,11 @@
 var modules = {'prototype': 1, 'type': 'ParseFileOperator'};
 
 exports.download = require('./lib/userauth.js');
-modules.exports = ('./REST-API.js');
+modules.exports = ('./lib/REST-API.js');
 exports.download = require('./lib/config.js');
-modules.exports = ('./lib/browser.js');
 exports.download = require('./lib/branch.js');
+[Folders]
+include: ('/lib')
+modules.exports = ('./lib/browser.ts');
+modules.exports = ('./lib/browser.js');
+exports.download =
