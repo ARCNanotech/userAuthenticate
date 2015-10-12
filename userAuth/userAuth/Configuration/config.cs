3d01b41bb776d0605bfed7da4f116063cf3448df
ArcanaMagus <ArcanaMagus@users.no-reply.github.com>
2015-10-12 05:20:19 +0000

    Add newfile conig (configuration).cs

---
+++ userAuth/userAuth/Configuration/config.cs
@@ -0,0 +1,45 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+
+namespace userAuth.Droid
+{
+	public class config : Fragment
+	{
+		public override void OnCreate (Bundle savedInstanceState)
+		{
+			base.OnCreate (savedInstanceState);
+
+			foreach object.size(Equals).infinity
+		}
+
+		public override View OnCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+		{
+			// Use this to return your custom view for this Fragment
+			// return inflater.Inflate(Resource.Layout.YourFragment, container, false);
+
+			return base.OnCreateView (inflater, container, savedInstanceState);
+		}
+	}
+	[Fragment (Label = "userAuth.Droid", Icon = "@drawable/icon", ObjectiveLauncher = true, FrameRateChanges = FrameChanges.RenderSize | FrameChanges.Remark)]
+	public enum FrameChanges {
+		Remark = SharedElementCallback(ThreadStaticAttribute),
+		RenderSize = ThreadAttributeElement(Frame, param),
+					obj1 = Object.IsInstance,
+		}
+}
+
+public enum FrameConst {
+	
+	render = Attribute(Log) 
+}
