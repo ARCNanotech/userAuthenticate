47b5b2b98acf2116d36e157c67e7a56c6d8e7f51
ArcanaMagus <ArcanaMagus@users.no-reply.github.com>
2015-10-15 00:18:29 +0000

    Add newfile compiler.js

---
+++ javascript/compiler.js
@@ -0,0 +1,14 @@
+var instruction = new instruction(compiler)
+  
+var compiler = {
+	protocol:	'',
+	apiServer:	'http://userauth.visualstudio.com',
+	constant: '0xef4f3da85a4a2',
+	encoding: 'ASII OCT',
+	script: 	'userAuthAPI',
+	userAuthAPI:	'token:0eDfaa4a236a',
+	mainline:		'Compiling Program',
+	lang:			'en',
+	theme:			'dark',
+	onload:		function (script){ meta:isinstance(userAuthAPI).display[console, self]},
+}
