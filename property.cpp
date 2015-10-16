561556346dcc9fc62fe289446ec585b2a63d55d7
ArcanaMagus <ArcanaMagus@users.no-reply.github.com>
2015-10-16 02:34:23 +0000

    Modified file property.cpp

--- property.cpp
+++ property.cpp
@@ -10,5 +10,24 @@ project element static[object(slider) {
 } if [[element.slider: IOerror(pre: set, re-set: center)].post:'./makefile'];
 else [[ property.element(construct::meta, _init_, propert: null, event: (ev:EventArgs()))
 		]]
+	  try:
+	  const InitUI init;
+
 
 }
+namespace userAuth
+{
+	[SystemProperty]
+	 public class Property
+	 {
+		SystemProperty property;
+
+		[define]
+		 public enum ForEachProperty ()
+		 {
+			const property = DefineProperty.Android.StoreValue ();
+		 }
+	 };
+
+}
+
