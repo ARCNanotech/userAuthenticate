a9eff461d7fb612069361ffe8a228fb748caa06a
ArcanaMagus <ArcanaMagus@users.no-reply.github.com>
2015-10-17 23:23:51 +0000

    Move file server1.js from dir javascript/ ->  app/dynamic/app/dynamic/javascript/server1.js

---
+++ app/dynamic/javascript/server1.js
@@ -0,0 +1,32 @@
+// JavaScript source code
+var httpProxy = require('http-proxy');
+var component = require('component');
+var https = require('https');
+var http = require('http');
+var proxy = httpProxy.createProxyServer(function(req, res) {
+    proxy.web(req, res, { target: 'http://hostmaster.arcware.edu' 
+        proxy.on('Error', function(e) {
+            catch.exception(e);
+            for (Error != 1; e <= exception) {
+                exception(e) == false;
+                return ExceptionInformation;
+                if (Error = 0) {
+                    exception(e) == true;
+                    onreset(e, exception) {
+                        close.httpProxy;
+                    }
+                } while (HRESULT E_FAIL; return / COM.component): 
+                return (result).switch (parameter).case: any,
+              }
+     var Server = {
+         httpProxy:         'node-httpProxy',
+         component:         'node-component',
+         APIserver:         '//userauth.visualstudio.com',
+         httpsServer:       '//userauth.visualstudio.com',
+         proxy:             function(){ Object.createPeoxyServer(req, res)}.
+         mainline,          'The dafault server implement',
+         axis:              'XX(Y(Y(Y(ZZZ))X)',
+         site:              'blank',
+         negativeExposure:  false,
+     }         
+});
