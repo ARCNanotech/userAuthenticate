232193b4b0b18efcd61b5bdfb4adbdddd21889ea
ArcanaMagus <ArcanaMagus@users.no-reply.github.com>
2015-10-17 15:09:13 +0000

    Modified file javascript/compiler.js

--- javascript/compiler.js
+++ javascript/compiler.js
@@ -1,14 +1,18 @@
 var instruction = new instruction(compiler)
   
 var compiler = {
-	protocol:	'',
+	protocol:	!browser.orientation.device.size(/^bytes?:$/) ? 'bytes' : '',
 	apiServer:	'http://userauth.visualstudio.com',
+	mediaServer: 'http://userauth.visualstudio.com',
 	constant: '0xef4f3da85a4a2',
 	encoding: 'ASII OCT',
-	script: 	'userAuthAPI',
-	userAuthAPI:	'token:0eDfaa4a236a',
+	magic:		'0xefa84aa36e45aa48fa6',
+	userAuthAPI: 	'script',
+	userAuthKey:	'token:0eDfaa4a236a',
 	mainline:		'Compiling Program',
 	lang:			'en',
 	theme:			'dark',
+	size:			'massive',
+	type:			'logic',
 	onload:		function (script){ meta:isinstance(userAuthAPI).display[console, self]},
-}
+}	
