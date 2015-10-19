bf1f151eb92c4dbcc92abd30d6768457dd2b5648
ArcanaMagus <ArcanaMagus@users.no-reply.github.com>
2015-10-19 21:24:03 +0000

    Modify File app/dynamic/app/scripts/test.sh

--- app/dynamic/app/scripts/test.sh
+++ app/dynamic/app/scripts/test.sh
@@ -2,7 +2,7 @@ ls -v
 git status -sb
 git add app/dynamic/app/scripts/test.sh
 .\app/dynamic/app/scripts/test.sh
-git commit -m "Add newfile app/dynamic/app/scripts/test.sh"
+git commit -m "Modify File app/dynamic/app/scripts/test.sh"
 git push origin userAuth
 git push origin HEAD:userAuth
 git pull origin userAuth
@@ -28,6 +28,12 @@ sleep 10
 git push origin HEAD:HTML
 git pull origin HTML
 sleep 22
+git push origin HEAD:debugger
+git pull origin debugger
+sleep 13
+git push origin HEAD:debug
+git pull origin debug
+sleep 15
 git push origin HEAD:ArcanaMagus-debugger
 git pull orgin ArcanaMagus-debugger
 sleep 7
@@ -36,12 +42,18 @@ git pull origin readme
 sleep 12
 git push origin HEAD:userAuth.api
 git pull origin userAuth.api
+sleep 14
+git push origin HEAD:userAuth.app
+git pull origin userAuth.app
 sleep 5
 git push origin HEAD:userAuth.cpl
 git pull origin userAuth.cpl
 sleep 34
 git push origin HEAD:api
 git pull origin api
+sleep 11
+git push origin HEAD:build
+git pull origin build
 sleep 30
 git push origin HEAD:binary
 git pull origin binary
@@ -51,12 +63,17 @@ git pull origin Mocha
 sleep 18
 git push origin HEAD:project
 git pull origin project
+sleep 10
+git push orgin HEAD:javascript
+git pull origin javascript
 sleeo 17
 git push origin HEAD:package
 git pull origin package
 sleep 45
 git push origin HEAD:ArcanaMagus-HTML
 git pull orgin ArcanaMagus-HTML
+git push origin HEAD:Readme-1
+git pull origin Readme-1
 git diff -b
 git status -sb
 git checkout build
@@ -83,7 +100,16 @@ const: eval: frame(in).shell
 }fi
 while { const: err().args }
 for {
-	var init = console.print(err at 
+var location = System.Cli.Pointer.Value(X, Y, Z)
+{
+`X = (location.length);
+`Y = (location.width);
+`Z = (location.height);
+}
+	var init = console.print(err at) 
 	err = static.frame_exception(meta::init)
 console.err(args) args="none"; frame_exception(in console::meta)
+case: "init":  return init(err:location[X((0), X(1), Y(0), Z(0))])
+case "err": return err(init: location[Y((1), Y(0), Z(0))]) 
+
 	}
