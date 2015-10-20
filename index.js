f5918921118b648fa5cdf6965ba3945d4cb4fe01
ArcanaMagus <ArcanaMagus@users.no-reply.github.com>
2015-10-20 20:28:52 +0000

    Modify File index.js

--- index.js
+++ index.js
@@ -9,4 +9,6 @@ exports.download = require('./lib/branch.js');
 include: ('/lib')
 modules.exports = ('./lib/browser.ts');
 modules.exports = ('./lib/browser.js');
-exports.download = ('./lib/config.js');
+exports.download = require('./lib/config.js');
+module.exports.download = require("./lib/connect.js");
+module.exports.dependency = require("./lib/connect.ts");
